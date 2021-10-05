972fc5349ba66dfb2688661ace55880770730bad
kbus9455 <katarzyna.bus9455@gmail.com>
2021-10-05 19:17:12 +0200

    loading arrays form contact

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const addContactSubmitButton = document.querySelector('.addContactButtons button
 
 // Display window to add new contact 
 
-const addContact=()=>{
-addContactWindow.classList.toggle("active");
+const addContact = () => {
+    addContactWindow.classList.toggle("active");
 }
 
 addContactButton.addEventListener("click", addContact);
@@ -14,56 +14,93 @@ addContactButton.addEventListener("click", addContact);
 
 // Clear Inputs function 
 
-const clearInputs = function (){
-[...document.querySelectorAll('.addContact input')].forEach(input=>{
-    input.value="";
-})
+const clearInputs = function () {
+    [...document.querySelectorAll('.addContact input')].forEach(input => {
+        input.value = "";
+    })
 };
 
 // Hide window to add contact 
-addContactCancelButton.addEventListener('click', ()=>{
-addContactWindow.classList.toggle("active");
-clearInputs();
+addContactCancelButton.addEventListener('click', () => {
+    addContactWindow.classList.toggle("active");
+    clearInputs();
 })
 
-// adding contact 
+// loading contacts form array 
 
-let row = 1;
-const addContactSubmit =()=>{
-let name = document.getElementById("name").value;
-let phone = document.getElementById("phone").value;
-let address = document.getElementById("address").value;
-let city = document.getElementById("city").value;
+contactsData = [{
+        name: "Katarzyna",
+        phone: "123456789",
+        address: "Cicha 4",
+        city: "Poznan"
 
-if(!name || !phone || !address || !city){
-    alert("Fill in all fields");
-    return;
+    }, {
+        name: "Grzegorz",
+        phone: "9764756765",
+        address: "Jasna 5",
+        city: "Rzeszow"
+
+    }, {
+        name: "Janek",
+        phone: "775756756",
+        address: "Prosta 24",
+        city: "Warszawa"
+
+    }
+
+];
+loadTableData(contactsData);
+
+function loadTableData(contactsData){
+    const tableBody = document.getElementById('tableData');
+    let dataHtml = "";
+    for (let contact of contactsData){
+        dataHtml += `<tr><td>${contact.name}</td><td>${contact.address}</td><td>${contact.phone}</td><td>${contact.city}</td></tr>`
+    };
+    console.log(dataHtml);
+    tableBody.innerHTML = dataHtml;
 }
 
-const display  = document.getElementById("display");
-let newRow = display.insertRow(row);
 
-let cell1 = newRow.insertCell(0);
-let cell2 = newRow.insertCell(1);
-let cell3 = newRow.insertCell(2);
-let cell4 = newRow.insertCell(3);
 
-  localStorage.setItem("name", name);
-    localStorage.setItem("phone", phone);
-    localStorage.setItem("address", address);
-    localStorage.setItem("city", city);
+let row = 1;
+const addContactSubmit = () => {
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let address = document.getElementById("address").value;
+    let city = document.getElementById("city").value;
 
-cell1.innerHTML = localStorage.name;
-cell2.innerHTML = localStorage.phone;
-cell3.innerHTML= localStorage.address;
-cell4.innerHTML = localStorage.city;
+    if (!name || !phone || !address || !city) {
+        alert("Fill in all fields");
+        return;
+    }
 
-// let jsonObj = { 'row': cell1.innerHTML };
-//   localStorage.setItem('row', JSON.stringify(jsonObj));
-//   cell1 = localStorage.getItem('row');
-
-clearInputs();
-addContactWindow.classList.toggle("active");
+    clearInputs();
+    addContactWindow.classList.toggle("active");
 }
 
 addContactSubmitButton.addEventListener('click', addContactSubmit);
+
+
+
+    // const display = document.getElementById("display");
+    // let newRow = display.insertRow(row);
+
+    // // let cell1 = newRow.insertCell(0);
+    // // let cell2 = newRow.insertCell(1);
+    // // let cell3 = newRow.insertCell(2);
+    // // let cell4 = newRow.insertCell(3);
+
+    // // localStorage.setItem("name", name);
+    // // localStorage.setItem("phone", phone);
+    // // localStorage.setItem("address", address);
+    // // localStorage.setItem("city", city);
+
+    // // cell1.innerHTML = localStorage.name;
+    // // cell2.innerHTML = localStorage.phone;
+    // // cell3.innerHTML= localStorage.address;
+    // // cell4.innerHTML = localStorage.city;
+
+    // // let jsonObj = { 'row': cell1.innerHTML };
+    // //   localStorage.setItem('row', JSON.stringify(jsonObj));
+    // //   cell1 = localStorage.getItem('row');
