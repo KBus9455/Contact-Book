97afa980246b345a473ebf20ef6b73a7b6fcf7c8
kbus9455 <katarzyna.bus9455@gmail.com>
2021-10-18 22:47:52 +0200

    edit contact, add pencil icon, function: fill inputs,  get id of clicked element

--- main.js
+++ main.js
@@ -2,20 +2,30 @@ const addContactButton = document.querySelector('button.add');
 const addContactWindow = document.querySelector('.addContactWrapper');
 const addContactCancelButton = document.querySelector('.addContactButtons button');
 const addContactSubmitButton = document.querySelector('.addContactButtons button.btn-success');
+const editContactSubmitButton = document.querySelector('.addContactButtons button.btn-warning');
 const deleteContactButton = document.querySelector('button.delete');
 const display = document.querySelector("#display");
-let deleteButtonsArray = [...display.querySelectorAll("button")];
+let deleteButtonsArray;
+let editButtonsArray;
+let contactsData;
+
+// current item:
+let currentContact;
+
 
 // Display window to add new contact 
 
-const addContact = () => {
+const showAddContactWindow = () => {
     deleteButtonsArray.forEach(button => {
         button.classList.remove('active')
     });
+      document.querySelector("div.title").textContent = "Add contact"; 
+     editContactSubmitButton.style.display = "none";
+     addContactSubmitButton.style.display = "inline";
     addContactWindow.classList.toggle("active");
 }
 
-addContactButton.addEventListener("click", addContact);
+addContactButton.addEventListener("click", showAddContactWindow);
 
 // Clear Inputs function 
 
@@ -26,8 +36,10 @@ const clearInputs = function () {
 };
 
 // Hide window to add contact 
-addContactCancelButton.addEventListener('click', () => {
+addContactCancelButton.addEventListener('click', (e) => {
+    e.preventDefault();
     addContactWindow.classList.toggle("active");
+         editContactSubmitButton.style.display = "inline";
     clearInputs();
 })
 
@@ -38,17 +50,18 @@ loadTableData();
 function loadTableData() {
     const tableBody = document.getElementById('tableData');
     let itemsFromLocalStorage = localStorage.getItem('savedContacts');
-    let contactsData = JSON.parse(itemsFromLocalStorage);
+    contactsData = JSON.parse(itemsFromLocalStorage);
     let dataHtml = "";
     if (contactsData !== null) {
         let id = 1;
         for (let contact of contactsData) {
-            dataHtml += `<tr><td>${id}</td><td>${contact.name}</td><td>${contact.address}</td><td>${contact.phone}</td><td>${contact.city}</td><td><button type="button" class="btn btn-danger deleteInLine">X</button></td></tr>`
+            dataHtml += `<tr><td>${id}</td><td>${contact.name}</td><td>${contact.address}</td><td>${contact.phone}</td><td>${contact.city}</td><td><i class="fas fa-pencil-alt"></i></td><td><button type="button" class="btn btn-danger deleteInLine">X</button></td></tr>`
             id++;
         };
     }
     tableBody.innerHTML = dataHtml;
   deleteButtonsArray = [...display.querySelectorAll("button")];
+  editButtonsArray = [...document.querySelectorAll("i")];
 }
 
 let row = 1;
@@ -57,6 +70,8 @@ const addContactSubmit = () => {
     let phone = document.getElementById("phone").value;
     let address = document.getElementById("address").value;
     let city = document.getElementById("city").value;
+
+    editContactSubmitButton.style.display = "none";
 
     if (!name || !phone || !address || !city) {
         alert("Fill in all fields");
@@ -95,3 +110,33 @@ const removeElement = function(){
     let index;
     
 }
+
+
+// edit contact 
+const changeWindowToEditContact = function (){
+  document.querySelector("div.title").textContent = "Edit contact"; 
+  addContactSubmitButton.style.display = "none";
+  editContactSubmitButton.style.display = "inline";
+}
+
+const fillInputs = function (id){
+ document.querySelector("#name").value = contactsData[id].name;
+ document.querySelector("#phone").value = contactsData[id].phone;
+ document.querySelector("#address").value = contactsData[id].address;
+ document.querySelector("#city").value = contactsData[id].city;
+}
+
+
+const editContact = function (e){
+    e.preventDefault();
+showAddContactWindow();
+changeWindowToEditContact();
+const listId = e.target.parentNode.parentNode;
+const id = listId.querySelector("td").textContent;
+console.log(id);
+fillInputs(id);
+}
+
+editButtonsArray.forEach(function(button){
+button.addEventListener("click", editContact);
+})
